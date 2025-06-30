50a853fabdb5066f3f32075320e01748ee20c5f5
dilmurodivcc <zufarovamohira32@gmail.com>
2025-06-30 10:05:48 +0500

    FIX: Layout bag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,10 @@ import type { AppProps } from "next/app";
 import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
 import "../styles/globals.css";
+import { log } from "console";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  console.log("MyApp rendered with component:", Component.name);
   const isSimpleLayout =
     Component.name === "Services" ||
     Component.name === "Home" ||
